fdeeb350df77bc83dc1a90bdae3fc4dfc8b66381
leo <leo@leo-sa.com>
2013-12-21 22:51:20 -0600

    Created makeBlock

--- src/client/hxdebug/Injector.hx
+++ src/client/hxdebug/Injector.hx
@@ -1,7 +1,23 @@
 package ;
 import haxe.macro.Expr;
 class Injector {
-    public static function inject(exp: Expr) {
+    public static function inject(exp: Expr) : Expr {
+        return exp;
+    }
 
+    public static function makeBlock(exprs:Array<Expr>, pos:Position) : Expr {
+        var result = new Array<Expr>();
+        for (expr in exprs) {
+            var file = expr.pos.file;
+            var ex = macro trace("hit");
+            trace(ex);
+            result.push(ex);
+            result.push(inject(expr));
+        }
+
+        return {
+            expr: ExprDef.EBlock(result),
+            pos: pos
+        }
     }
 }
--- src/test/hxdebug/InjectorTest.hx
+++ src/test/hxdebug/InjectorTest.hx
@@ -1,4 +1,20 @@
+import haxe.macro.Expr;
 import haxe.unit.TestCase;
 class InjectorTest extends TestCase {
+    public function testMakeBlock() {
 
+        var exprArr = new Array<Expr>();
+        exprArr.push(macro var a = 1);
+        exprArr.push(macro var b = 1);
+        exprArr.push(macro if (a > b) trace("test"));
+        var block = Injector.makeBlock(exprArr, {file: "test.hx", min:0, max: 0});
+        switch (block.expr) {
+            case ExprDef.EBlock(exprs):
+                assertEquals(exprs.length, 6);
+            default:
+                assertTrue(false);
+        }
+        trace(block);
+
+    }
 }
