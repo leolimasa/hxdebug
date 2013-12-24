47f54c86bfc7d9fe67ae7e94895f08652b86d2d0
lsa <leo@leo-sa.com>
2013-12-23 21:42:54 -0600

    converted injector to an object

--- src/client/hxdebug/Injector.hx
+++ src/client/hxdebug/Injector.hx
@@ -1,14 +1,24 @@
 package hxdebug;
+import Std;
 import sys.io.File;
-import haxe.macro.Context;
-import haxe.PosInfos;
 import haxe.macro.ExprTools;
 import haxe.macro.Expr;
+
+/**
+* Changes arbitrary code such that there will be a Debugger.hit() call inside every
+* block.
+**/
 class Injector {
 
-    private static var files:Map<String,String> = new Map<String, String>();
+    private var files:Map<String,String> = new Map<String, String>();
 
-    public static function inject(exp: Expr) : Expr {
+    public function new() {
+
+    }
+
+    // ..................................................................................
+
+    public function inject(exp: Expr) : Expr {
         switch(exp.expr) {
             case ExprDef.EBlock(exprs):
                 return makeBlockFromExpr(exp);
@@ -18,36 +28,41 @@ class Injector {
         }
     }
 
-    // __________________________________________________________________________________
+    // ..................................................................................
 
     /**
     * Will return an Expr object that is equivalent of the following:
     *
     * Debugger.hit([file], [min], [max])
     **/
-    private static function makeInjectExpr(position: Position) {
+    public function makeInjectExpr(position: Position) {
         var file = position.file;
-        var min = position.min;
-        var max = position.max;
+        var line = findLineInFile(file, position.min);
 
+        // Construct function
         var functionName = macro hxdebug.Debugger.hit;
-        var functionParam = {
+        var functionParam1 = {
             expr: ExprDef.EConst(Constant.CString(file)),
             pos: position
         };
+        var functionParam2 = {
+            expr: ExprDef.EConst(Constant.CInt(Std.string(line))),
+            pos: position
+        };
 
+        // Return the full expression
         return {
             expr: ExprDef.ECall(
                 functionName,
-                [functionParam]
+                [functionParam1, functionParam2]
             ),
             pos: position
         }
     }
 
-    // __________________________________________________________________________________
+    // ..................................................................................
 
-    public static function makeBlock(exprs:Array<Expr>, pos:Position) : Expr {
+    public function makeBlock(exprs:Array<Expr>, pos:Position) : Expr {
         var result = new Array<Expr>();
         for (expr in exprs) {
             result.push(makeInjectExpr(expr.pos));
@@ -60,9 +75,9 @@ class Injector {
         }
     }
 
-    // __________________________________________________________________________________
+    // ..................................................................................
 
-    public static function makeBlockFromExpr(expr : Expr) : Expr {
+    public function makeBlockFromExpr(expr : Expr) : Expr {
         switch(expr.expr) {
             case ExprDef.EBlock(exprs):
                 return makeBlock(exprs, expr.pos);
@@ -71,13 +86,13 @@ class Injector {
         }
     }
 
-    // __________________________________________________________________________________
+    // ..................................................................................
 
     /**
     * Returns the count of how many linebreaks there are in contents before the specified
     * position.
     **/
-    public static function charPosToLine(contents:String, pos:Int) {
+    public function charPosToLine(contents:String, pos:Int) : Int {
         var stripped = contents.substr(0, pos);
         var count = 0;
         for (i in 0...stripped.length) {
@@ -88,12 +103,12 @@ class Injector {
         return count + 1;
     }
 
-    // __________________________________________________________________________________
+    // ..................................................................................
 
     /**
     * Returns which line number the specified character position is within a file.
     **/
-    public static function findLineInFile(file:String, pos:Int) {
+    public function findLineInFile(file:String, pos:Int) : Int {
         if (!files.exists(file)) {
             var f = File.getContent(file);
             files.set(file, f);
--- src/test/hxdebug/InjectorTest.hx
+++ src/test/hxdebug/InjectorTest.hx
@@ -7,14 +7,56 @@ import haxe.macro.Expr;
 import haxe.unit.TestCase;
 import haxe.CallStack;
 
+class MockInjector extends Injector {
+    public override function makeInjectExpr(position: Position) {
+        return macro trace("hi");
+    }
+}
+
 class InjectorTest extends TestCase {
+
+    var inj:Injector;
+
+    public override function setup() {
+        super.setup();
+        inj = new MockInjector();
+    }
+
+    public function testInject() {
+        var e = macro if (a > b) {
+            var c = 1;
+            var d = 2;
+            for (e in f) {
+                call1("arg");
+                call2("arg");
+            }
+            trace("test");
+        }
+        var expect = "if(a > b) {
+\ttrace(\"hi\");
+\tvar c = 1;
+\ttrace(\"hi\");
+\tvar d = 2;
+\ttrace(\"hi\");
+\tfor(e in f) {
+\t\ttrace(\"hi\");
+\t\tcall1(\"arg\");
+\t\ttrace(\"hi\");
+\t\tcall2(\"arg\");
+\t};
+\ttrace(\"hi\");
+\ttrace(\"test\");
+} ";
+        assertEquals(expect, ExprTools.toString(inj.inject(e)));
+    }
+
     public function testMakeBlock() {
         var exprArr = new Array<Expr>();
         exprArr.push(macro var a = 1);
         exprArr.push(macro var b = 1);
         exprArr.push(macro if (a > b) trace("test"));
 
-        var block = Injector.makeBlock(exprArr, {file: "test.hx", min:0, max: 0});
+        var block = inj.makeBlock(exprArr, {file: "test.hx", min:0, max: 0});
         switch (block.expr) {
             case ExprDef.EBlock(exprs):
                 assertEquals(exprs.length, 6);
@@ -27,22 +69,10 @@ class InjectorTest extends TestCase {
     public function testPosToLine() {
         var cont = "line1\r\nline2\r\nline3";
 
-        assertEquals(1, Injector.charPosToLine(cont, 2));
-        assertEquals(2, Injector.charPosToLine(cont, 7));
-        assertEquals(3, Injector.charPosToLine(cont, 15));
+        assertEquals(1, inj.charPosToLine(cont, 2));
+        assertEquals(2, inj.charPosToLine(cont, 7));
+        assertEquals(3, inj.charPosToLine(cont, 15));
     }
 
-    /*public function testInject() {
-        var e = macro if (a > b) {
-            var c = 1;
-            var d = 2;
-            for (e in f) {
-                call1("arg");
-                call2("arg");
-            }
-            trace("test");
-        }
-        trace(ExprTools.toString(Injector.inject(e)));
 
-    } */
 }
