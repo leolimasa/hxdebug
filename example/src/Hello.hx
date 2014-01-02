1c2776180047ee673a68a5da04db200c00e81d9a
lsa <leo@leo-sa.com>
2014-01-01 22:14:29 -0600

    builder injects everywhere in the classpath except the project being built

--- example/src/Hello.hx
+++ example/src/Hello.hx
@@ -2,7 +2,6 @@ import haxe.PosInfos;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 
-@:build(hxdebug.Builder.build())
 class Hello {
 
     public static function main() {
--- src/client/hxdebug/Builder.hx
+++ src/client/hxdebug/Builder.hx
@@ -1,28 +1,33 @@
 package hxdebug;
-import ClasspathTools;
-import hxdebug.tools.FileCache;
-import FileCache;
-import haxe.ds.StringMap;
-import sys.io.File;
-import sys.FileSystem;
-import haxe.macro.Type.ClassType;
-import haxe.macro.Compiler;
 import haxe.macro.Expr.Field;
+import haxe.macro.Compiler;
+import hxdebug.tools.ClasspathTools;
 import haxe.macro.Context;
 
 class Builder {
 
+    /**
+    * Cycles through every directory in the class path looking for classes to add a
+    * @:build annotation. Only classes with the specified packages will be processed.
+    * If no packages are specified, it will default to all packages.
+    *
+    **/
     public static function debug(?packages:Array<String>) {
-        if (packages == null) {
-            packages = [""];
-        }
+        if (packages == null) packages = [""];
 
-        var processedClasses = [];
+        var classPath:Array<String> = Context.getClassPath();
 
-        for (p in packages) {
-            for (cls in ClasspathTools.getClassesInPackage(p)) {
-                Compiler.addMetadata("@:build(hxdebug.Builder.build())", cls);
-                Compiler.keep(className, null, true);
+        for (cp in classPath) {
+
+            // don't search on root
+            if (cp == "/") continue;
+
+            for (p in packages) {
+                for (cls in ClasspathTools.getClassesInPackage(cp, p)) {
+                    trace(cls);
+                    Compiler.addMetadata("@:build(hxdebug.Builder.build())", cls);
+                    Compiler.keep(cls, null, true);
+                }
             }
         }
     }
@@ -34,7 +39,4 @@ class Builder {
         var injector = new Injector();
         return injector.injectFields(fields);
     }
-
-
-
 }
